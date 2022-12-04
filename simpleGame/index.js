ae5fb45caf0fd84a1f7ef445ccbffaff361be692
희진 <khj16355@naver.com>
2022-12-05 01:07:23 +0900

    :construction: 장애물 생성 및 이동
    장애물 생성 및 이동

--- simpleGame/index.js
+++ simpleGame/index.js
@@ -16,11 +16,18 @@ let player = {
   inplay: false
 };
 
+let pipe = {
+  startPos : 0,
+  spaceBetweenRow : 0,
+  spaceBetweenCol : 0,
+  pipeCount : 0
+}
+
 function start(){
   console.log('game start');
   player.inplay = true;
   player.score = 0;
-  gameArea.innerHTML= 0;
+  gameArea.innerHTML = '';
   gameMessage.classList.add('hide');
   startBtn.classList.add('hide');
   let bird = document.createElement('div');
@@ -35,14 +42,71 @@ function start(){
   player.x = bird.offsetLeft;
   player.y = bird.offsetTop;
 
+  pipe.startPos = 0;
+  pipe.spaceBetweenRow = 400;
+  //화면 넓이에 따라 파이프 갯수 변하도록
+  pipe.pipeCount = Math.floor(gameArea.offsetWidth / pipe.spaceBetweenRow);
+  
+  for(let i = 0; i < pipe.pipeCount; i++){
+    console.log('pipe.pipeCount',pipe.pipeCount)
+    makePipe(pipe.startPos * pipe.spaceBetweenRow);
+    pipe.startPos++;
+  }
+
   //움직임을 매개변수로 넣어준다음 부드럽게 처리해준다
   window.requestAnimationFrame(playGame);
 }
+
+function makePipe(pipePos) {
+  let totalHeight = gameArea.offsetHeight;
+  let totalWidth = gameArea.offsetWidth;
+  let pipeUp = document.createElement("div");
+  pipeUp.classList.add('pipe');
+  pipeUp.height = Math.floor(Math.random() * 350);
+  pipeUp.style.height = pipeUp.height + 'px';
+  pipeUp.style.left = totalWidth + pipePos + 'px';
+  pipeUp.x = totalWidth + pipePos;
+  pipeUp.style.top = '0px';
+  pipeUp.style.backgroundColor = 'red';
+
+  gameArea.appendChild(pipeUp);
+
+  pipe.spaceBetweenCol = Math.floor(Math.random()*250) + 150;
+
+
+  let pipeDown = document.createElement("div");
+  pipeDown.classList.add('pipe');
+  pipeDown.style.height = totalHeight - pipeUp.height - pipe.spaceBetweenCol + 'px'
+  pipeDown.style.left = totalWidth + pipePos + 'px';
+  pipeDown.x = totalWidth + pipePos;
+  pipeDown.style.bottom = '0px';
+  pipeDown.style.backgroundColor = 'black';
+  gameArea.appendChild(pipeDown);
+}
+
+function movePipes(){
+  let pipes = document.querySelectorAll(".pipe");
+  let counter = 0;
+  pipes.forEach(function(item){
+    item.x -= player.speed;
+    item.style.left = item.x = 'px';
+    if(item.x < 0){
+      item.parentElement.removeChild(item);
+      counter++;
+    }
+  });
+
+  for(let i = 0; i < counter/2 ; i++){
+    makePipe(0);
+  }
+}
+
 
 function playGame(){
   if(player.inplay){
     let bird = document.querySelector(".bird");
     let wing = document.querySelector(".wing")
+    movePipes();
     let move = false;
     if(keys.ArrowLeft && player.x > 0){
       player.x -= player.speed;
